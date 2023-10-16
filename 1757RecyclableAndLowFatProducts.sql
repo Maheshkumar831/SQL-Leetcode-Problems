bbf834a5ae06219a936fb784d85f532213172f70
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2023-10-16 05:59:43 +0530

    Create 1757RecyclableAndLowFatProducts.sql

---
+++ 1757RecyclableAndLowFatProducts.sql
@@ -0,0 +1,33 @@
+SELECT PRODUCT_ID FROM PRODUCTS WHERE LOW_FATS='Y' AND RECYCLABLE='Y';
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+/*Write a solution to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Only products 1 and 3 are both low fat and recyclable.*/
