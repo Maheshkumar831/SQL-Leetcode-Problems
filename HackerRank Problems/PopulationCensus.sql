58bc30237c61c309c1ac36056215d9348521c875
Mahesh Kumar R <114941303+Maheshkumar831@users.noreply.github.com>
2023-10-25 14:14:49 +0530

    Create PopulationCensus.sql

---
+++ HackerRank Problems/PopulationCensus.sql
@@ -0,0 +1,14 @@
+SELECT SUM(CITY.POPULATION) FROM CITY 
+JOIN COUNTRY
+ON CITY.COUNTRYCODE=COUNTRY.CODE
+WHERE CONTINENT='ASIA';
+
+/*--------------------------------------------------------------------------------
+Given the CITY and COUNTRY tables, query the sum of the populations of all cities where the CONTINENT is 'Asia'.
+
+Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
+
+Input Format
+
+The CITY and COUNTRY tables are described as follows:
+*/
